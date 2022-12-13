79e684d0706aac995cbddc5cb581021c5b9486e5
Brian2233 <brian@kuma-gaming.com>
2022-12-13 23:38:42 +0800

    first commit

---
+++ pages/products/[id].tsx
@@ -0,0 +1,9 @@
+
+
+function Detail(props:any) {
+  return (
+    <div>Detail Page</div>
+  )
+}
+
+export default Detail
---
+++ pages/products/index.tsx
@@ -0,0 +1,41 @@
+import Head from "next/head";
+import { CSSProperties } from "react";
+import styles from "../../styles/Home.module.css";
+
+interface InnerProps {
+  products: any[];
+}
+
+export default function index({ products }: InnerProps) {
+  // https://fakestoreapi.com/products
+//   console.log("products:", products);
+  const Product = ({ id,title }: any) => (
+    <div>
+      <h1>{id} {title}</h1>
+    </div>
+  );
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <a href="/">HOME</a>
+      </h1>
+      {products.length > 0
+        ? products.map((product) => (
+            <Product key={product.id} id={product.id} title={product.title}></Product>
+          ))
+        : null}
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const data = await res.json();
+  // console.log("products:",res.data);
+  return {
+    props: {
+      products: data,
+    },
+  };
+}
